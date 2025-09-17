c02bb6b1fa6550ca13850508d2a44018a003fd2e
khollow1 <owenken82@gmail.com>
2025-09-17 11:31:00 -0400

    dernier version controleur hackathon

--- src/main/java/cal/info/ControleurHackathon.java
+++ src/main/java/cal/info/ControleurHackathon.java
@@ -8,15 +8,101 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ControleurHackathon implements HttpHandler {
-
     private List<Hackathon> lesHackathons = new ArrayList<>();
 
+    public ControleurHackathon() {
+        lesHackathons.add(new Hackathon("HackQC", "Montréal"));
+        lesHackathons.add(new Hackathon("HackConcordia", "Montréal"));
+        lesHackathons.add(new Hackathon("HackUQam", "Montréal"));
+    }
+
     @Override
-    public void handle(HttpExchange echange) throws IOException {
-        String response = "Bienvenue au controle des hackathons !";
-        echange.sendResponseHeaders(200, response.length());
-        OutputStream os = echange.getResponseBody();
+    public void handle(HttpExchange exchange) throws IOException {
+        String typeRequete = exchange.getRequestMethod();
+        String chemin = exchange.getRequestURI().getPath();
+        String query = exchange.getRequestURI().getQuery();
+
+        System.out.println("Requête reçue : " + typeRequete + " " + chemin + (query != null ? "?" + query : ""));
+
+        switch (typeRequete) {
+            case "GET":
+                if (query != null && query.contains("nom=")) {
+                    rechercheHackathons(exchange, query);
+                } else {
+                    listeHackathons(exchange);
+                }
+                break;
+
+            case "POST":
+                ajouterHackathon(exchange);
+                break;
+
+            case "PUT":
+                modifierHackathon(exchange);
+                break;
+
+            case "DELETE":
+                supprimerHackathon(exchange);
+                break;
+
+            default:
+                sendResponse(exchange, "Méthode HTTP non supportée : " + typeRequete, 405);
+                break;
+        }
+    }
+
+    private void sendResponse(HttpExchange exchange, String response, int statusCode) throws IOException {
+        exchange.sendResponseHeaders(statusCode, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
+    }
+
+    private void listeHackathons(HttpExchange exchange) throws IOException {
+        String texte = "Liste des hackathons:\n";
+        for (int i = 0; i < lesHackathons.size(); i++) {
+            Hackathon h = lesHackathons.get(i);
+            texte += h.getNom() + " - " + h.getLieu() + "\n";
+        }
+        sendResponse(exchange, texte, 200);
+    }
+
+    private void rechercheHackathons(HttpExchange exchange, String query) throws IOException {
+        String nomRecherche = query.replace("nom=", "");
+        String texte = "Résultats pour " + nomRecherche + ":\n";
+        for (int i = 0; i < lesHackathons.size(); i++) {
+            Hackathon h = lesHackathons.get(i);
+            if (h.getNom().equalsIgnoreCase(nomRecherche)) {
+                texte += h.getNom() + " - " + h.getLieu() + "\n";
+            }
+        }
+        sendResponse(exchange, texte, 200);
+    }
+
+    private void ajouterHackathon(HttpExchange exchange) throws IOException {
+        Hackathon nouveau = new Hackathon("HackNew", "VilleX");
+        lesHackathons.add(nouveau);
+        String texte = "Hackathon ajouté: " + nouveau.getNom() + " - " + nouveau.getLieu();
+        sendResponse(exchange, texte, 201);
+    }
+
+    private void modifierHackathon(HttpExchange exchange) throws IOException {
+        if (lesHackathons.size() > 0) {
+            Hackathon h = lesHackathons.get(0);
+            h.setNom("HackModifie");
+            h.setLieu("VilleY");
+            sendResponse(exchange, "Hackathon modifié : " + h.getNom() + " - " + h.getLieu(), 200);
+        } else {
+            sendResponse(exchange, "Aucun hackathon à modifier", 404);
+        }
+    }
+
+    private void supprimerHackathon(HttpExchange exchange) throws IOException {
+        if (lesHackathons.size() > 0) {
+            Hackathon h = lesHackathons.remove(0);
+            sendResponse(exchange, "Hackathon supprimé : " + h.getNom(), 200);
+        } else {
+            sendResponse(exchange, "Aucun hackathon à supprimer", 404);
+        }
     }
 }
--- src/main/java/cal/info/Hackathon.java
+++ src/main/java/cal/info/Hackathon.java
@@ -1,3 +1,33 @@
 package cal.info;
+
 public class Hackathon {
+    private String nom;
+    private String lieu;
+
+   
+    public Hackathon() {
+        this.nom = "";
+        this.lieu = "";
+    }
+
+    public Hackathon(String nom, String lieu) {
+        this.nom = nom;
+        this.lieu = lieu;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
 }
